915d874b70f46da4239e34d5ad7189396d8d369f
Mateo Arboleda Roldán <marteomasoyyo@gmail.com>
2019-03-05 14:07:31 -0500

    Borro ambiente virtual

--- ambiente_virtual/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\cca\desktop\frameworkgamificacion\ambiente_virtual\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

d3dabff0788cdcf1ee86eaa8a85d5d26a37237c1
Mateo Arboleda Roldán <marteomasoyyo@gmail.com>
2019-03-13 20:29:42 -0500

    Starting login

--- framework/urls.py
+++ framework/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('evaluacion', views.evaluacion, name='evaluacion'),
     path('resultados', views.resultados, name='resultados'),
-    path('registrar', views.register, name='register')
+    path('registrar', views.register, name='register'),
+    path('login', views.login, name='login')
 ]

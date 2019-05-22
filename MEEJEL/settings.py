29cc2ef4c327ffa74e87bd4d42936eda0d535c35
Mateo Arboleda <marteomasoyyo@gmail.com>
2019-05-21 21:37:59 -0500

    Try fix images

--- MEEJEL/settings.py
+++ MEEJEL/settings.py
@@ -124,7 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Here on the options are not in the file by default
 

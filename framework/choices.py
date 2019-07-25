62a76a2018516f438d224341d27c9dfc7dd29845
Cristian CA <cc.acosta00@gmail.com>
2019-07-25 13:03:24 -0500

    Listo para el video
    .

--- framework/choices.py
+++ framework/choices.py
@@ -1,28 +1,28 @@
 GRADE_CHOICES = (
-    (1, "Null"),
-    (2, "Medium"),
-    (3, "Significant"),
-    (4, "High"),
-    (5, "Very High")
+    (1, "Nulo"),
+    (2, "Medio"),
+    (3, "Significativo"),
+    (4, "Alto"),
+    (5, "Muy Alto")
 )
 
 PRINCIPLE_CHOICES = (
-    (1, "Orientation"),
-    (2, "Persuasive elements"),
-    (3, "Learning orientation"),
-    (4, "Achievement based rewards"),
-    (5, "Generation adaptable"),
-    (6, "Amusement factors"),
-    (7, "Transformative"),
-    (8, "Wellbeing oriented"),
-    (9, "Research generating"),
-    (10, "Knowledge-based")
+    (1, "Orientación"),
+    (2, "Elementos persuasivos"),
+    (3, "Orientación de aprendizaje"),
+    (4, "Recompensas basadas en logros"),
+    (5, "Logros adaptables"),
+    (6, "Factores de diversión"),
+    (7, "Transformador"),
+    (8, "Orientado al bienestar"),
+    (9, "Generando investigación"),
+    (10, "Basado en el conocimiento")
 )
 
 PRINCIPLES_TYPES_CHOICES = (
     (1, "Roles"),
-    (2, "Materials"),
-    (3, "Steps"),
-    (4, "Rules"),
-    (5, "Goals"),
+    (2, "Materiales"),
+    (3, "Pasos"),
+    (4, "Reglas"),
+    (5, "Metas"),
 )
